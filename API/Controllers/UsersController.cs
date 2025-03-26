505dbb9e5cd56ce50ca17d15c925fa232945201f
waqar <dev.syedwaqar@gmail.com>
2025-03-27 02:00:31 +0500

    walking skeleton - API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class UsersController(DataContext context) : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+            var users = await context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task <ActionResult<IEnumerable<AppUser>>> GetUser(int id){
+            var user = await context.Users.FindAsync(id);
+            if (user == null) return NotFound();
+            return Ok(user);
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+var app = builder.Build();
+
+app.MapControllers();
+app.Run();
