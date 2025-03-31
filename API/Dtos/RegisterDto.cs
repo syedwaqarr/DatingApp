27ddc9970435a9344612907657d5121e519cdcb5
waqar <dev.syedwaqar@gmail.com>
2025-03-31 15:14:18 +0500

    error-handling

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -6,7 +6,9 @@ namespace API.Dtos;
 public class RegisterDto
 {
     [Required]
-    public required string Username { get; set; }
-    public required string Password {get;set;}
+    public string Username { get; set; } = string.Empty;
+    [Required]
+    [StringLength(8, MinimumLength = 4)]
+    public string Password { get; set; } = string.Empty;
 
 }
--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using API.Data;
 using API.Extensions;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -17,6 +18,7 @@ builder.Services.AddIdentityService(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200", "http://localhost:4200"));
 
 app.UseAuthentication();
